8c7cd4e5a8c7a2e2ce49bf6ecda15007e016d781
Aman Chauhan <amangotpro@gmail.com>
2022-02-12 22:56:15 +0530

    Style: material_Ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import { useState } from 'react';
 import './App.css';
+import { FormControl, formControl, Input, InputLabel } from '@mui/material'
 
 function App() {
   const [todos, setTodos] = useState(['Take dogs for walk', 'rubbish out', 'it is fkcinggg true']);
   const [input, setInput] = useState('');
 
   const addTodo = (event) => {
+    event.preventDefault();
     setTodos([...todos, input]);
+    setInput('');
   }
 
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input value={input} onChange={event => setInput(event.target.value)} />
-      <button onClick={addTodo}>Add todo</button>
+
+      
+
+      <form>
+        <FormControl>
+        <InputLabel>✅Write a Todo</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)} />
+        </FormControl>
+
+        {/* <input value={input} onChange={event => setInput(event.target.value)} /> */}
+        <button disabled={!input} className="btn btn-primary" type='submit' onClick={addTodo}>Add todo</button>
+      </form>
+      
 
       <ul>
         {todos.map(todo => (
+          
           <li>{todo}</li>
         ))}
       </ul>
---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+
+
+function Todo() {
+    return (
+        <div>
+            <li>{todo}</li>
+        </div>
+)
+}
+
+export default Todo
