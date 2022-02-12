a718a7eba3b6ffab8acb1c8b5ad357d795874a56
Aman Chauhan <amangotpro@gmail.com>
2022-02-12 22:03:50 +0530

    Update: added functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function App() {
   const [todos, setTodos] = useState(['Take dogs for walk', 'rubbish out', 'it is fkcinggg true']);
   const [input, setInput] = useState('');
 
+  const addTodo = (event) => {
+    setTodos([...todos, input]);
+  }
+
 
   return (
     <div className="App">
       <h1>Hello</h1>
       <input value={input} onChange={event => setInput(event.target.value)} />
-      <button>Add todo</button>
+      <button onClick={addTodo}>Add todo</button>
 
       <ul>
         {todos.map(todo => (
